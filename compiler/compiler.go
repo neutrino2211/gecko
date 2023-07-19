8da1a04ba13bb620e64e6596bdef41549ab484ed
neutrino2211 <neutrino2211@gmail.com>
2023-07-19 01:30:00 +0100

    Print only LLIR

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -117,6 +117,15 @@ func Compile(file string, config *config.CompileCfg) string {
 
 	os.WriteFile(outName, []byte(llir), 0o755)
 
+	if config.Ctx.Bool("ir-only") {
+		cmd := exec.Command("cp", outName, ".")
+		err := streamCommand(cmd)
+
+		if err != nil {
+			ast.ErrorScope.NewCompileTimeError("LLIR copy", "Error copying LLVM IR "+err.Error(), lexer.Position{})
+		}
+	}
+
 	llcArgs := []string{"-filetype=obj"}
 
 	if ast.Config.Arch == "arm64" && ast.Config.Platform == "darwin" {

4be6537cb6ca251dd5ae891272cb6a533527201d
neutrino2211 <neutrino2211@gmail.com>
2023-11-19 20:49:10 +0100

    Add README

--- main.go
+++ main.go
@@ -16,7 +16,10 @@ func main() {
 
 	cmd := &cli.App{
 		Commands:    commands.GeckoCommands,
-		Description: "A playful new language written in Go",
+		Description: "Gecko is a programming language designed for writing low level and highly performant applications using a beginner friendly syntax.",
+		Name:        "gecko",
+		HelpName:    "gecko",
+		Usage:       "A playful new language written in Go",
 	}
 
 	if err := cmd.Run(os.Args); err != nil {
